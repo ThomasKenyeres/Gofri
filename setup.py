270363f3f1fa2caf1fc3bbd5639cf794ca226b68
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-24 20:39:40 +0100

    Installer

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Gofri',
+    version='0.1.0',
+    packages=['build.lib.gofri', 'build.lib.gofri.lib', 'build.lib.gofri.lib.pip', 'build.lib.gofri.lib.xml',
+              'build.lib.gofri.lib.conf', 'build.lib.gofri.lib.decorate', 'gofri', 'gofri.lib', 'gofri.lib.pip',
+              'gofri.lib.xml', 'gofri.lib.conf', 'gofri.lib.decorate', 'gofri.lib.project_generator'],
+    url='',
+    license='',
+    author='thomas',
+    author_email='',
+    description=''
+)
