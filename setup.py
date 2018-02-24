2a6325a8043f4e18b5c8eaa39c8d583fd2c94333
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-24 21:16:42 +0100

    Corrected packages

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from distutils.core import setup
 setup(
     name='Gofri',
     version='0.1.0',
-    packages=['build.lib.gofri', 'build.lib.gofri.lib', 'build.lib.gofri.lib.pip', 'build.lib.gofri.lib.xml',
-              'build.lib.gofri.lib.conf', 'build.lib.gofri.lib.decorate', 'gofri', 'gofri.lib', 'gofri.lib.pip',
+    packages=['gofri', 'gofri.lib', 'gofri.lib.pip',
               'gofri.lib.xml', 'gofri.lib.conf', 'gofri.lib.decorate', 'gofri.lib.project_generator'],
+    install_requires=[
+        'flask',
+        'flask_restful',
+        'sqlalchemy',
+        'importlib',
+        'xmltodict'
+    ],
     url='',
     license='',
     author='thomas',
