8d75ae22e4cb593821a83f764ce895ac8334f5a5
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-25 22:08:48 +0100

    Simplified package generation + corrections

--- gofri/lib/project_generator/generator.py
+++ gofri/lib/project_generator/generator.py
@@ -12,6 +12,10 @@ def get_project_name(name):
             root_package += "_"
     return root_package.lower()
 
+def make_python_packages(path):
+    os.makedirs(path, exist_ok=True)
+    open("{}/__init__.py".format(path), "w")
+
 
 def generate_start_file(root_package, name):
     with open("{}/{}".format(root_package, "start.py"), "w") as start_file:
@@ -31,19 +35,17 @@ def generate_xml(root_package, name):
         xml_file.write(build_xml(root_package, name))
 
 def generate_web_dir(root_package):
-    os.makedirs("{}/{}".format(root_package, "web"), exist_ok=True)
-    open("{}/{}/{}".format(root_package, "web", "__init__.py"), "w")
+    make_python_packages("{}/{}".format(root_package, "web"))
 
 def generate_back_dir(root_package):
-    os.makedirs("{}/{}/{}".format(root_package, "back", "controller"), exist_ok=True)
-    open("{}/{}/{}".format(root_package, "back", "__init__.py"), "w")
+    make_python_packages("{}/{}".format(root_package, "back"))
+    make_python_packages("{}/{}/{}".format(root_package, "back", "controller"))
 
 def generate_project(path, name, web=True, back=True, db=True, orm=True, custom_xml=False):
     root_package_name = get_project_name(name)
     root_package = "{}/{}/{}".format(path, name, root_package_name)
 
-    os.makedirs(root_package, exist_ok=True)
-    open("{}/{}".format(root_package, "__init__.py"), "w")
+    make_python_packages(root_package)
 
     generate_xml(root_package, name)
     generate_start_file(root_package, name)
