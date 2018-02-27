b7ba24882095edec0ce1e21389085d2bc2d81cc8
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-27 23:05:00 +0100

    Argument evaluation migrated to cli_nodes.py

--- gofri/lib/project_generator/cli.py
+++ gofri/lib/project_generator/cli.py
@@ -1,39 +1,11 @@
-from gofri.lib.project_generator.module_generator import generate_module
+from gofri.lib.project_generator.cli_nodes import data, RootNode
 
 
 def execute_command(project_root_package, argv):
     command_words = argv[1::]
     print("COMMANDS: {} from {}".format(command_words, project_root_package))
 
-    #TODO: beautify cli argparse
+    root_path = project_root_package + "/"
+    data["root"] = root_path
 
-    try:
-        root_path = project_root_package + "/"
-        command = command_words[0]
-        if command == "generate":
-            option = command_words[1]
-            if option == "project":
-                print("GEN project")
-            elif option == "module":
-                name = command_words[2]
-                path = ""
-                if len(command_words) == 4:
-                    path = command_words[3]
-                    generate_module(
-                        root_package_path=root_path,
-                        inner_path=path,
-                        name=name
-                    )
-                    print("Generated module {} at {}".format(name, path))
-            elif option == "controller":
-                name = command_words[2]
-                generate_module(
-                    root_package_path=root_path,
-                    inner_path="back/controller",
-                    name=name
-                )
-                print("Generated controller {}".format(name))
-            else:
-                raise IndexError()
-    except IndexError:
-        print("Invalid input!")
+    RootNode()
