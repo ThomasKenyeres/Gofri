75c5ef03a53f6b7dd1b056714c47b663312a4b5d
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-27 11:20:54 +0100

    Unused line deleted

--- gofri/lib/decorate/tools.py
+++ gofri/lib/decorate/tools.py
@@ -4,7 +4,6 @@ from inspect import signature
 def generate_arg_tuple(function, path_arg_tuple, request_args):
     selected = []
     f_signature = tuple(str(val) for val in signature(function).parameters.values())
-    f_signature_end = f_signature[-len(request_args)::] if len(f_signature) > len(path_arg_tuple) else tuple()
     for arg in request_args:
         if arg in f_signature:
             selected.append(request_args[arg])
