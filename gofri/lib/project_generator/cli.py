3fdfbd830a6ffbb3d4f00bd4dda13eb112eb6214
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-28 11:15:43 +0100

    new data stored from input

--- gofri/lib/project_generator/cli.py
+++ gofri/lib/project_generator/cli.py
@@ -1,3 +1,5 @@
+import os
+
 from gofri.lib.project_generator.cli_nodes import data, RootNode
 
 
@@ -7,5 +9,6 @@ def execute_command(project_root_package, argv):
 
     root_path = project_root_package + "/"
     data["root"] = root_path
+    data["root_base"] = os.path.basename(project_root_package)
 
     RootNode()
