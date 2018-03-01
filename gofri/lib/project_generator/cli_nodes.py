d7373a3edd14f3d3d55c38de8e27e3767fea80c8
ThomasKenyeres <kenyerest97@gmail.com>
2018-03-01 12:05:54 +0100

    New generator features implemented

--- gofri/lib/project_generator/cli_nodes.py
+++ gofri/lib/project_generator/cli_nodes.py
@@ -1,4 +1,6 @@
 from clinodes.nodes import ArgNode
+from gofri.lib.project_generator.shortcuts import generate_controller
+
 from gofri.lib.project_generator.module_generator import generate_module2
 
 data = {}
@@ -25,24 +27,56 @@ class ControllerGeneratorNode(ArgNode):
     def run(self, *args_remained):
         name = args_remained[0]
         inner_path = "{}.back.controller".format(data["root_base"])
-        print("FUUCK: {}".format(inner_path))
         generate_module2(
             root_package_path=data["root"],
             module_package=inner_path,
             name=name
         )
 
+
+class ModelGeneratorNode(ArgNode):
+    def setup(self):
+        self.expects_more = False
+        self.enable_any = True
+
+    def run(self, *args_remained):
+        name = args_remained[0]
+        inner_path = "{}.back.model".format(data["root_base"])
+        generate_module2(
+            root_package_path=data["root"],
+            module_package=inner_path,
+            name=name
+        )
+
+
+class ServiceGeneratorNode(ArgNode):
+    def setup(self):
+        self.expects_more = False
+        self.enable_any = True
+
+    def run(self, *args_remained):
+        name = args_remained[0]
+        inner_path = "{}.back.service".format(data["root_base"])
+        generate_module2(
+            root_package_path=data["root"],
+            module_package=inner_path,
+            name=name
+        )
+
+
 class GenerateNode(ArgNode):
     def setup(self):
         self.commands = {
             "module" : ModuleGeneratorNode,
-            "controller": ControllerGeneratorNode
+            "controller": ControllerGeneratorNode,
+            "model": ModelGeneratorNode,
+            "service": ServiceGeneratorNode
         }
         self.expects_more = True
 
 class RootNode(ArgNode):
     def setup(self):
         self.commands = {
-            "generate": GenerateNode
+            "generate": GenerateNode,
         }
         self.expects_more = True
