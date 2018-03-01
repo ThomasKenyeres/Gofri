536112903b1c6a147ba31c8e268beb8ceaaff654
ThomasKenyeres <kenyerest97@gmail.com>
2018-03-01 15:21:56 +0100

    new package added: gofri.lib.util

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=['gofri', 'gofri.lib', 'gofri.lib.pip', 'gofri.lib.xml',
               'gofri.lib.conf', 'gofri.lib.decorate',
-              'gofri.lib.project_generator'],
+              'gofri.lib.project_generator', 'gofri.lib.util'],
     install_requires=[
         'flask',
         'flask_restful',
