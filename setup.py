2b3fcd7a56731b5dad94cc3ae488a60a7f76f027
ThomasKenyeres <kenyerest97@gmail.com>
2018-03-02 10:48:34 +0100

    added gofri.lib.http

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='0.1.0',
     packages=['gofri', 'gofri.lib', 'gofri.lib.pip', 'gofri.lib.xml',
               'gofri.lib.conf', 'gofri.lib.decorate',
-              'gofri.lib.project_generator', 'gofri.lib.util'],
+              'gofri.lib.project_generator', 'gofri.lib.util',
+              'gofri.lib.http'
+              ],
     install_requires=[
         'flask',
         'flask_restful',
