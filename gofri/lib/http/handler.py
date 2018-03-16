2313848c064495b86e41d34c88d2fd8a549b393b
ThomasKenyeres <kenyerest97@gmail.com>
2018-03-16 14:57:00 +0100

    Final args

--- gofri/lib/http/handler.py
+++ gofri/lib/http/handler.py
@@ -1,6 +1,6 @@
 class RequestHandler(object):
-    def __init__(self, headers="", body="", json=""):
+    def __init__(self, headers="", json="", params=""):
         self.path = None
         self.headers = [param.strip() for param in headers.split(";")]
-        self.body = [param.strip() for param in body.split(";")]
         self.json = [param.strip() for param in json.split(";")]
+        self.params = [param.strip() for param in params.split(";")]
