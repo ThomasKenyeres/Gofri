1f2e8877bb2e4df01c66fcf5805bb791f39451cc
ThomasKenyeres <kenyerest97@gmail.com>
2018-03-20 22:06:56 +0100

    All http decorator action implemented here

--- gofri/lib/http/handler.py
+++ gofri/lib/http/handler.py
@@ -1,6 +1,43 @@
+from gofri.lib.main import APP
+
 class RequestHandler(object):
-    def __init__(self, headers="", json="", params=""):
-        self.path = None
-        self.headers = [param.strip() for param in headers.split(";")]
-        self.json = [param.strip() for param in json.split(";")]
-        self.params = [param.strip() for param in params.split(";")]
+    def __init__(self, path, request="", response_type="text/plain", headers="", params=""):
+        self.path = path
+        self.headers = headers
+        self.params= params
+        self.request = request
+        self.response_type = response_type
+
+    def __call__(self, func):
+        return APP.set_url_endpoint(
+            self.path, func, ["GET"],
+            request_nm=self.request,
+            params_nm=self.params,
+            headers_nm=self.headers,
+            response_type=self.response_type
+        )
+
+class PostBasedRequestHandler(RequestHandler):
+    def __init__(self, path, request="", response_type="text/plain",
+                 headers="", params="", body="", json="", json_obj=""):
+        RequestHandler.__init__(self, path, request=request, response_type=response_type,
+                                headers=headers, params=params)
+        self.body = body
+        self.json = json
+        self.json_obj = json_obj
+        self.__methods = self._set_methods()
+
+    def __call__(self, func):
+        return APP.set_url_endpoint(
+            self.path, func, self.__methods,
+            request_nm=self.request,
+            params_nm=self.params,
+            headers_nm=self.headers,
+            body_nm=self.body,
+            json_nm=self.json,
+            json=self.json_obj,
+            response_type=self.response_type
+        )
+
+    def _set_methods(self):
+        pass
