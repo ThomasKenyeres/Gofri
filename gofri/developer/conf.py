5b7957ccd20bdb3d22e0a13853cb51ef0e677a79
ThomasKenyeres <kenyerest97@gmail.com>
2018-03-22 12:24:42 +0100

    Developer tools

---
+++ gofri/developer/conf.py
@@ -0,0 +1,27 @@
+import configparser
+
+from gofri.lib.globals import init_conf
+from gofri.lib.main import CONF
+
+
+class LocalConfigIO(object):
+    def __init__(self, filename="custom-conf.ini"):
+        self.__local_conf_path = "{}/{}".format("", filename)
+        self.__config = configparser.ConfigParser()
+        self.update()
+
+    def update(self):
+        self.__config.read(self.__local_conf_path)
+
+    def __getitem__(self, item):
+        return self.__config[item]
+
+    def __setitem__(self, key, value):
+        self.__config[key] = value
+
+    def commit(self):
+        with open(self.__local_conf_path, "w") as conf_file:
+            self.__config.write(conf_file)
+
+    def config(self):
+        return self.__config.sections()
