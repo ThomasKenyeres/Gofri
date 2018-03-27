0edde1eb91b368311620a62568ed11ce875f9609
ThomasKenyeres <kenyerest97@gmail.com>
2018-03-27 14:23:56 +0200

    Deleted unused functions and corrected some references

--- gofri/lib/main.py
+++ gofri/lib/main.py
@@ -14,40 +14,8 @@ import gofri.lib.globals as GLOB
 from gofri.lib.http.app import Application
 from gofri.lib.pip.pip_handler import PIPHandler
 
-ROOT = ""
-ROOT_PATH = ""
-
-C = XMLConfigReader(ROOT_PATH)
-
-CONF = None
-HOST = None
-PORT = None
-DATABASE_RDBMS = None
-MYSQL_CONFIG = None
-DEPENDENCIES = None
-CUSTOM_MODULES = None
-AUTO_PIP = False
-
-def init_config():
-    global CONF, HOST, PORT, DATABASE_RDBMS, MYSQL_CONFIG, DEPENDENCIES, CUSTOM_MODULES
-
-    CONF = C.get_conf_xml()["configuration"]
-    HOST = C.get_dict_config(CONF, "hosting", "host")
-    PORT = C.get_dict_config(CONF, "hosting", "port")
-    DATABASE_RDBMS = C.get_dict_config(CONF, "database", "rdbms")
-    MYSQL_CONFIG = C.get_dict_config(CONF, "database", "mysql-config")
-    DEPENDENCIES = C.get_dict_config(CONF, "dependencies", "dependency")
-    CUSTOM_MODULES = C.get_dict_config(CONF, "custom-modules", "module")
-
-    if isinstance(DEPENDENCIES, str):
-        DEPENDENCIES = [DEPENDENCIES]
-
-    if isinstance(CUSTOM_MODULES, str):
-        CUSTOM_MODULES = [CUSTOM_MODULES]
-
-
 def init_custom_config(filename):
-    fullpath = "{}/{}".format(ROOT_PATH, filename)
+    fullpath = "{}/{}".format(GLOB.Config().ROOT_PATH, filename)
     conf = ConfigParser()
     conf.read(fullpath)
     return conf
@@ -65,8 +33,8 @@ Base = declarative_base()
 
 def integrate_custom_modules():
     #TODO: Check more cases
-    if CUSTOM_MODULES is not None:
-        for cmod in CUSTOM_MODULES:
+    if GLOB.Config().CUSTOM_MODULES is not None:
+        for cmod in GLOB.Config().CUSTOM_MODULES:
             if isinstance(cmod, str):
                 runpy.run_module("{}.main".format(cmod), run_name="__main__", alter_sys=True)
 
@@ -86,7 +54,7 @@ def main(root_path, modules):
 
     GLOB.init_conf(root_path)
     piphandler = PIPHandler()
-    piphandler.package_names = DEPENDENCIES
+    piphandler.package_names = GLOB.Config().DEPENDENCIES
 
     piphandler.install()
     print("All required dependencies are installed")
