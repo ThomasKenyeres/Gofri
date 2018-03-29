36223661d589a33bb119542351bdfcffb9acdd7f
ThomasKenyeres <kenyerest97@gmail.com>
2018-03-29 11:47:45 +0200

    New functions implemented

--- gofri/developer/conf.py
+++ gofri/developer/conf.py
@@ -2,6 +2,10 @@ import configparser
 
 from gofri.lib.globals import Config
 
+class SectionWrapper:
+    def __init__(self, name, confdict):
+        self.name = name
+        self.confdict = confdict
 
 class LocalConfigIO(object):
     def __init__(self, filename="local.ini"):
@@ -24,3 +28,23 @@ class LocalConfigIO(object):
 
     def config(self):
         return self.__config.sections()
+
+    def add(self, sectionw):
+        self.__config[sectionw.name] = sectionw
+
+    def add_raw(self, string):
+        with open(self.__local_conf_path, "a") as conf_file:
+            conf_file.write(string)
+
+    def merge(self, source, to_file=None):
+        #if Config().EXT_CONF_ENABLE_CIO:
+        if to_file is None:
+            to_file = "{}/{}".format(Config().ROOT_PATH, "local.ini")
+        if True:
+            with open(self.__local_conf_path, "a") as targetf:
+                content = ""
+                with open(self.__local_conf_path, "r") as sourcef:
+                    content = sourcef.read()
+                targetf.write(content)
+        #if Config().EXT_CONF_ENABLE_CIO:
+            #with open(self.__local_conf_path, )
