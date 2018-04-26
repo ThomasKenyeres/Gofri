fbc1d670aeee5d2dae5b7010ad92c5cf1ae168ba
ThomasKenyeres <kenyerest97@gmail.com>
2018-04-26 22:17:43 +0200

    Package handling reimplemented(more configurability)

--- gofri/lib/pip/pip_handler.py
+++ gofri/lib/pip/pip_handler.py
@@ -1,6 +1,8 @@
 import importlib
 import os
 
+import pip
+
 
 def check_pname(name):
     #TODO: check if package is available
@@ -10,21 +12,35 @@ def check_pname(name):
 
 class PIPHandler():
     def __init__(self):
-        self.package_names = []
+        self.packages = []
 
     def get_uninstalled_packages(self):
         result = []
-        if self.package_names is not None:
-            for pname in self.package_names:
+        if self.packages is not None:
+            for package in self.packages:
+                pname = package["name"]
                 spec = importlib.util.find_spec(pname)
                 if spec is None:
-                    result.append(pname)
+                    result.append(package)
         return result
 
 
     def install(self):
-        for package_name in self.get_uninstalled_packages():
+        for package in self.get_uninstalled_packages():
+            package_name = package["name"]
+            install_scope = package["install"]
+            version = package["version"]
+            if version == "latest":
+                version = None
             if check_pname(package_name):
                 print("Installing missing dependency: \"{}\" ...".format(package_name))
-                print("> FLASK: ", end="")
-                os.system("sudo -S pip install " + package_name)
+                if install_scope == "user":
+                    if version is not None:
+                        os.system("pip install {}=={} --user".format(package_name, version))
+                    else:
+                        os.system("pip install {} --user".format(package_name))
+                if install_scope == "system":
+                    if version is not None:
+                        os.system("sudo -S pip install {}=={}".format(package_name, version))
+                    else:
+                        os.system("sudo -S pip install " + package_name)
