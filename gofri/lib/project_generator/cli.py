b1e79c6d1ab8bbec11fc9cd95b8b8017b3deaac3
ThomasKenyeres <kenyerest97@gmail.com>
2018-04-30 23:52:24 +0200

    Detect difference between file- and shell based command

--- gofri/lib/project_generator/cli.py
+++ gofri/lib/project_generator/cli.py
@@ -3,9 +3,14 @@ import os
 from gofri.lib.project_generator.cli_nodes import data, RootNode
 
 
-def execute_command(project_root_package, argv):
-    root_path = project_root_package + "/"
-    data["root"] = root_path
-    data["root_base"] = os.path.basename(project_root_package)
+def execute_command(project_root_package, argv, from_file=True):
+    if from_file:
+        root_path = project_root_package + "/"
+        data["root"] = root_path
+        data["root_base"] = os.path.basename(project_root_package)
+    else:
+        root_path = os.getcwd()
+        data["root"] = root_path
+        data["root_base"] = os.path.basename(os.getcwd())
 
     RootNode()
