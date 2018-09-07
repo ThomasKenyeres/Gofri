816a3502e969bfe1a81b23bfc51edf2a33869dba
thomas <kenyerest97@gmail.com>
2018-09-07 15:46:01 +0200

    Skeleton defined

--- gofri/lib/decorate/orm.py
+++ gofri/lib/decorate/orm.py
@@ -1,2 +1,9 @@
-class Entity:
-    pass
+class GofriEntity:
+    def __init__(self, static=True):
+        self.static = static
+
+    def __call__(self, cls):
+        if self.static:
+            pass
+        else:
+            pass
