0fbbafe9f5c9c573da8c9e0d227891480ccdb6b9
thomas <kenyerest97@gmail.com>
2018-10-05 16:29:58 +0200

    Refactored

--- gofri/lib/application/__init__.py
+++ gofri/lib/application/__init__.py
@@ -3,20 +3,20 @@ import os
 from gofri.lib.http.app import Application
 
 
-class WSGI:
+class HttpDrivers:
     GOFRI = "gofri"
     FLASK = "flask"
 
 class ApplicationWrapper(object):
-    def __init__(self, wsgi=WSGI.GOFRI):
-        self.wsgi = wsgi
-        if wsgi == WSGI.GOFRI:
+    def __init__(self, driver_for=HttpDrivers.GOFRI):
+        self.wsgi = driver_for
+        if driver_for == HttpDrivers.GOFRI:
             self.wsgi_application = Application(static_conf={
                 "enable": True,
                 "dir": "/static",
                 "path": os.path.join(os.path.dirname(__file__), "static"),
             })
-        elif wsgi == WSGI.FLASK:
+        elif driver_for == HttpDrivers.FLASK:
             pass
 
     def start(self, host="0.0.0.0", port=8080):
