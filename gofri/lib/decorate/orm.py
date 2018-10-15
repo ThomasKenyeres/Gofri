fe54a9b4b1da3c263f0d103b1495c2476ccbd923
ThomasKenyeres <kenyerest97@gmail.com>
2018-10-15 22:02:17 +0200

    Revert "Bug 2018 10 08 jinja2 (with previous commits)"

--- gofri/lib/decorate/orm.py
+++
@@ -1,9 +0,0 @@
-class GofriEntity:
-    def __init__(self, static=True):
-        self.static = static
-
-    def __call__(self, cls):
-        if self.static:
-            pass
-        else:
-            pass
